76028e3f284da0489e114b0c6ccf516640e0c561
Padmakumar P B <padmakumarpb176@gmail.com>
2024-07-18 12:04:48 +0530

    Improved code structure

--- caesar_cipher_algorithm.py
+++ caesar_cipher_algorithm.py
@@ -4,30 +4,44 @@ alphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n
 def encryption(plain_text,shift_key):
     cipher_text = ""
     for char in plain_text:
-        position = alphabets.index(char)
-        new_position = ( position + shift_key ) % 26
-        cipher_text = cipher_text + alphabets[new_position]
-    print(f"The encrypted message is : {cipher_text}.")
+        if char in alphabets:
+            position = alphabets.index(char)
+            new_position = ( position + shift_key ) % 26
+            cipher_text = cipher_text + alphabets[new_position]
+        else :
+            cipher_text += char
+    return cipher_text
 
 
 def decryption(cipher_text,shift_key):
     plain_text = ""
     for char in cipher_text:
-        position = alphabets.index(char)
-        new_position = (position - shift_key ) % 26
-        plain_text = plain_text + alphabets[new_position] 
-    print(f"The decrypted message is : {plain_text}.")
+        if char in alphabets:
+            position = alphabets.index(char)
+            new_position = (position - shift_key ) % 26
+            plain_text = plain_text + alphabets[new_position] 
+        else :
+            plain_text += char
+    return plain_text
 
 
-ask_the_user = input("Type 'encrypt' for Encryption and 'decrypt' for Decryption\n")
+end_session = True
+while end_session:
+    ask_the_user = input("Type 'encrypt' for Encryption and 'decrypt' for Decryption\n").lower()
 
-text = input("Enter the message : ")
+    text = input("Enter the message : ").lower()
 
-shift_value = int(input("Enter the shift value : "))
+    shift_value = int(input("Enter the shift value : "))
 
+    if ask_the_user == "encrypt":
+        encrypted_message = encryption(text,shift_value)
+        print(f"The encrypted message is : {encrypted_message}")
+    elif ask_the_user == "decrypt":
+        decrypted_message = decryption(text,shift_value)
+        print(f"The decrypted message is : {decrypted_message}")
 
-if ask_the_user == "encrypt":
-    encryption(text,shift_value)
-elif ask_the_user == "decrypt":
-    decryption(text,shift_value)
+    choose  = input("Type 'yes' to continue and 'no' to stop\n").lower()
+    if choose == 'no':
+        end_session = False
+        print("Thank you. Have a nice day!\n")
 
